aafa0d30c5896be8c6cd44a423926dfb8c30ff7b
Den Abad <dennis.abad6@gmail.com>
2023-03-23 18:35:34 +1100

    Code cleanup for TextFaceCamera

--- Assets/TextFaceCamera.cs
+++ Assets/TextFaceCamera.cs
@@ -20,15 +20,6 @@ public class TextFaceCamera : MonoBehaviour
 
     private void Update()
     {
-        //transform.LookAt(cam.transform);
-        //transform.Rotate(new Vector3(x, y, z));
-
-        //Vector3 targetVector = cam.transform.position - transform.position;
-        //float newY = Mathf.Atan2(targetVector.z, targetVector.x) * Mathf.Rad2Deg;
-
-        //Mathf.
-
-        //transform.rotation = Quaternion.Euler(0, -1 * newY, 0);
         transform.forward = cam.transform.forward;
     }
 }
